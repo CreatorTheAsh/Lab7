58e88344e149ba8d870766943af035c74f1ed661
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2018-12-31 16:13:26 +0200

    Update Source.cpp
    Добавил минимальное значение между соседними элементами массива.

--- Source.cpp
+++ Source.cpp
@@ -13,10 +13,17 @@ int Bigger(int a, int b) {
 	else { c = b; }
 	return c;
 }
+int Smaller(int a, int b) {
+	int c;
+	if (a < b) { c = a; }
+	else { c = b; }
+	return c;
+}
 
 int main() {
-	int n, k, l, max;
+	int n, k, l, max, min;
 	int MAX = 0;
+	int MIN = 0;
 	cout << "n: "; cin >> n;
 	int *B = new int [n];
 	for (int p = 0; p < n;p++) {
@@ -26,9 +33,12 @@ int main() {
 		k = Dif(B[i], B[i + 1]);
 		l = Dif(B[i+1], B[i + 2]);
 		max = Bigger(k, l);
+		min = Smaller(k, l);
 		if (MAX < max) { MAX = max; }
+		if (MIN < min) { MiN = min; }
 	}
 		cout << "MAX: " << MAX << "\n";
+		cout << "MIN: " << MIN << "\n";
 		system("pause");
 		return 0;
 }
